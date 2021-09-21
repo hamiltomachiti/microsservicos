6d8ff404a1f616a79faee606b2a4b8b295f347c4
hamiltomachiti <hamiltomachiti@yahoo.com.br>
2021-09-20 22:14:11 -0300

    inclusão dos microsserviços de Barram.  e Consultas

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 app.use(bodyParser.json());
 const lembretes = {};
@@ -9,12 +11,22 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 });
 
-app.put('/lembretes', (req, res) => {
+app.put('/lembretes', async(req, res) => {
     contador++;
     const { texto } = req.body;
-    lembretes[contador] = { contador, texto }
+    lembretes[contador] = { contador, texto };
+
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: { contador, texto },
+    });
 
     res.status(201).send(lembretes[contador]);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
 });
 
 app.listen(4040, () => {
